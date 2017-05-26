c24a5a256bab87f1e1f1ac58689ac9d1332cf9da
Amay Dubey <amaydubey@gmail.com>
2017-05-25 17:09:15 -0700

    Application update

--- src/main/java/com/baeldung/spring/controller/JobApplicationController.java
+++ src/main/java/com/baeldung/spring/controller/JobApplicationController.java
@@ -3,17 +3,34 @@
  */
 package com.baeldung.spring.controller;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.baeldung.spring.dao.InterestedDao;
 import com.baeldung.spring.dao.JobApplicationDao;
@@ -49,6 +66,8 @@ public class JobApplicationController {
 	
 	@Autowired
 	InterestedDao interestedDao;
+	
+	private static String UPLOADED_FOLDER = "C://";
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String applyPage(@RequestParam("userId") String jobSeekerId, @RequestParam("jobId") String jobId, Model model){
@@ -66,31 +85,43 @@ public class JobApplicationController {
 	 */
 	@RequestMapping(value = "/apply", method = RequestMethod.POST)
 	public String apply(@RequestParam("userId") String jobSeekerId, @RequestParam("jobId") String jobId,
-			@RequestParam("resumeFlag") boolean resumeFlag, @RequestParam("resumePath") String resumePath, Model model) {
-		JobApplication ja = new JobApplication();
-		ja = jobAppDao.apply(Integer.parseInt(jobSeekerId), Integer.parseInt(jobId), resumeFlag, resumePath);
-		JobSeeker js = jobSeekerDao.getJobSeeker(Integer.parseInt(jobSeekerId));
-		JobPosting jp = jobDao.getJobPosting(Integer.parseInt(jobId));
-		emailService.sendSimpleMessage(js.getEmailId(),
-				"You have successfully applied to the position " + jp.getTitle() + " at "
-						+ jp.getCompany().getCompanyName(),
-				"Hi " + js.getFirstName() + " " + js.getLastName()
-						+ ".\n You have successfully completed your application for " + jp.getTitle() + " at "
-						+ jp.getCompany().getCompanyName() + ".\n Regards,\nThe FindJobs Team");
-		Company company = jp.getCompany();
-		List<?> ij = interestedDao.getAllInterestedJobId(Integer.parseInt(jobSeekerId));
-		int i = 0;
-		if(ij.contains(Integer.parseInt(jobId))){
-			i = 1;
+			@RequestParam("resumeFlag") boolean resumeFlag, @RequestParam("resumePath") String resumePath,@RequestParam("file") Optional<MultipartFile> file, Model model) {
+		if(resumeFlag == true){
+			
+			
+			
+		return "userjobprofile";
+			
+			
+		}
+		else{
+			JobApplication ja = new JobApplication();
+			ja = jobAppDao.apply(Integer.parseInt(jobSeekerId), Integer.parseInt(jobId), resumeFlag, resumePath);
+			JobSeeker js = jobSeekerDao.getJobSeeker(Integer.parseInt(jobSeekerId));
+			JobPosting jp = jobDao.getJobPosting(Integer.parseInt(jobId));
+			emailService.sendSimpleMessage(js.getEmailId(),
+					"You have successfully applied to the position " + jp.getTitle() + " at "
+							+ jp.getCompany().getCompanyName(),
+					"Hi " + js.getFirstName() + " " + js.getLastName()
+							+ ".\n You have successfully completed your application for " + jp.getTitle() + " at "
+							+ jp.getCompany().getCompanyName() + ".\n Regards,\nThe FindJobs Team");
+			Company company = jp.getCompany();
+			List<?> ij = interestedDao.getAllInterestedJobId(Integer.parseInt(jobSeekerId));
+			int i = 0;
+			if(ij.contains(Integer.parseInt(jobId))){
+				i = 1;
+			}
+			
+			model.addAttribute("job", jp);
+			model.addAttribute("seeker", js);
+			model.addAttribute("company", company);
+			model.addAttribute("interested", i);
+			model.addAttribute("applied", 1);
+			
+			return "userjobprofile";
+			
 		}
 		
-		model.addAttribute("job", jp);
-		model.addAttribute("seeker", js);
-		model.addAttribute("company", company);
-		model.addAttribute("interested", i);
-		model.addAttribute("applied", 1);
-		
-		return "userjobprofile";
 	}
 
 	/**
@@ -120,5 +151,62 @@ public class JobApplicationController {
 		}
 		return "modified";
 	}
+	
+	//***************************************************
+	@RequestMapping("/viewResume")
+    public void downloadPDFResource( HttpServletRequest request, HttpServletResponse response, @RequestParam("Id") int id ) throws IOException
+    	{		
+			JobApplication j1 =jobAppDao.getJobApplication(id);
+			String path= j1.getResumePath();
+		
+			File file = new File(path);
+			System.out.println(file);
+				
+			if (file.exists()) 
+			{
+			System.out.println("File Found");	
+			response.setContentType("application/pdf");
+			response.addHeader("Content-Disposition", String.format("attachment; filename=\"" + file.getName() +"\""));
+			InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+			 
+	        //Copy bytes from source to destination(outputstream in this example), closes both streams.
+	        FileCopyUtils.copy(inputStream, response.getOutputStream());
+			
+			}
+
+    	
+    	
+    }
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,RedirectAttributes redirectAttributes) {
+    	
+    	if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+            System.out.println(path);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
+	
+	//****************************************************
 
 }
